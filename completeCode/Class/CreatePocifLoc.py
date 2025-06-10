da3aabd6733a22f5049b07db419000757377ee7c
bjh <lalasin2002@gmail.com>
2025-06-10 18:32:18 +0900

    개선후 정리

--- completeCode/Class/CreatePocifLoc.py
+++ completeCode/Class/CreatePocifLoc.py
@@ -1,97 +1,190 @@
 import maya.cmds as cmds
-
+from collections import OrderedDict
 
 class CreatePocifLoc:
-    def __init__(self  ):
-        self.Crv = None
-        self.CrvShp = None
-        self.Locs = []
-        self.PocifNodes = []
-        self.Points_for_makeCrv = []
-        self.__CountAddPoint = 0
-        self.MainName = ""
-        self.SuffixName = ""
-    
-    def Add_ObjForMakeCrvPos(self , Obj , Printing = False):
-        PrintString = ""
-        if isinstance(Obj , str) and cmds.objExists(Obj):
-            Pos = tuple(cmds.xform(Obj , q =1 , t = 1 , ws =1))
-            self.Points_for_makeCrv.append(Pos)
-            self.__CountAddPoint +=1
-            PrintString += "> AddCount{num}, obj : {var} , Pos : {pos} \n" .format(num = self.__CountAddPoint , var = Obj , pos = Pos )   
-        elif isinstance(Obj , list):
-            for x in Obj:
+    def __init__(self, main_name="Pocif_Crv", suffix_name="_Pocif", loc_suffix="_loc"):
+        self.crv = None
+        self.crv_shp = None
+        self.locs = []
+        self.pocif_nodes = []
+        self.points_for_make_crv = []
+        self.count_add_point = 0
+        self.main_name = main_name
+        self.suffix_name = suffix_name
+        self.loc_suffix = loc_suffix
+        self.pocif_dic = OrderedDict()
+        self.log_messages = []
+
+    def log(self, msg, print_now=False):
+        self.log_messages.append(msg)
+        if print_now:
+            print(msg)
+
+    def add_object_positions(self, obj, print_log=False):
+        if isinstance(obj, str) and cmds.objExists(obj):
+            pos = tuple(cmds.xform(obj, q=True, t=True, ws=True))
+            self.points_for_make_crv.append(pos)
+            self.count_add_point += 1
+            self.log("> AddCount {}, obj: {}, Pos: {}".format(self.count_add_point, obj, pos), print_now=print_log)
+        elif isinstance(obj, (list, tuple)):
+            for x in obj:
+                if not isinstance(x, str):
+                    raise TypeError(">> Invalid list element type: expected string object name, got {}".format(type(x)))
                 if cmds.objExists(x):
-                    Pos = tuple(cmds.xform(x , q =1 , t = 1 , ws =1))
-                    self.Points_for_makeCrv.append(Pos)
-                    self.__CountAddPoint +=1
-                    PrintString += "> AddCount{num}, obj : {var} , Pos : {pos} \n" .format(num = self.__CountAddPoint , var = x , pos = Pos )
-        else:
-            raise TypeError( ">>  Invalid object type. Expected a string or a list.")
-        if Printing:
-            print (PrintString)
-
-    def Add_PosForMakeCrvPos(self ,Pos ,Printing = False):
-        PrintString = ""
-        if (isinstance(Pos , tuple) and len(Pos) ==3 and all(isinstance(x ,(int, float)) for x in Pos) ):
-            self.Points_for_makeCrv.append(Pos)
-            self.__CountAddPoint +=1
-            PrintString += "> AddCount{num}, Pos : {pos} \n" .format(num = self.__CountAddPoint , pos = Pos )
-        elif isinstance (Pos , list):
-            for element in Pos:
-                if (isinstance(element , tuple) and len(element) ==3 and all(isinstance(x ,(int, float)) for x in element) ):
-                    self.Points_for_makeCrv.append(element)
-                    self.__CountAddPoint +=1
-                    PrintString += "> AddCount{num}, Pos : {pos} \n" .format(num = self.__CountAddPoint , pos = element )
+                    pos = tuple(cmds.xform(x, q=True, t=True, ws=True))
+                    self.points_for_make_crv.append(pos)
+                    self.count_add_point += 1
+                    self.log("> AddCount {}, obj: {}, Pos: {}".format(self.count_add_point, x, pos), print_now=print_log)
                 else:
-                    raise TypeError(">> Invalid element in list. Expected a tuple like (0, 0, 0), got: {} " .format(element))
-                    
+                    self.log(">> Object does not exist: {}".format(x), print_now=print_log)
         else:
-            raise TypeError(">> Invalid input. Expected a tuple like (0, 0, 0) or a list of such tuples.")
-        if Printing:
-            print (PrintString)
+            raise TypeError(">> Invalid object type. Expected a string or a list/tuple of strings.")
 
-    def MakeCrv(self , Name = "", Degree = 1):
-        self.MainName = "Pocif_Crv"
-        if len(self.Points_for_makeCrv)>1:
-            if Name == "":
-                Name = self.MainNamee
-                Count = 1
-                while cmds.objExists(Name):
-                    Name = "{}{}".format(self.MainName , Count)
-                    Count += 1
-
-            self.Crv = cmds.curve(n = Name , d =Degree  , p = self.Points_for_makeCrv)
-            self.CrvShp = cmds.listRelatives(self.Crv , s =1)[0]
+    def add_positions(self, pos_list, print_log=False):
+        if (isinstance(pos_list, tuple) and len(pos_list) == 3 and all(isinstance(x, (int, float)) for x in pos_list)):
+            self.points_for_make_crv.append(pos_list)
+            self.count_add_point += 1
+            self.log("> AddCount {}, Pos: {}".format(self.count_add_point, pos_list), print_now=print_log)
+        elif isinstance(pos_list, (list, tuple)):
+            for element in pos_list:
+                if (isinstance(element, tuple) and len(element) == 3 and all(isinstance(x, (int, float)) for x in element)):
+                    self.points_for_make_crv.append(element)
+                    self.count_add_point += 1
+                    self.log("> AddCount {}, Pos: {}".format(self.count_add_point, element), print_now=print_log)
+                else:
+                    raise TypeError(">> Invalid element in list. Expected a tuple like (0, 0, 0), got: {}".format(element))
         else:
-            raise TypeError(">> Not enough points to create curve. At least 2 points are required.")
-        
+            raise TypeError(">> Invalid input. Expected a tuple like (0, 0, 0) or a list/tuple of such tuples.")
 
-    def MakePocifs(self , Name = "" , Suffix = "_Pocif"):
-        BaseName = ""
-        NodeName = None
-        if self.Crv and self.CrvShp:
-            if Name == "":
-                BaseName = self.Crv
+    def define_crv(self, curve, normalization=True):
+        crv = None
+        crv_shp = None
+        if isinstance(curve, str):
+            obj_type = cmds.objectType(curve)
+            if obj_type == "transform":
+                shapes = cmds.listRelatives(curve, s=True, type="nurbsCurve")
+                if shapes:
+                    crv = curve
+                    crv_shp = shapes[0]
+                else:
+                    raise ValueError(">> No nurbsCurve shape found under transform {}".format(curve))
+            elif obj_type == "nurbsCurve":
+                parents = cmds.listRelatives(curve, p=True, type="transform")
+                if parents:
+                    crv_shp = curve
+                    crv = parents[0]
+                else:
+                    raise ValueError(">> No transform parent found for nurbsCurve {}".format(curve))
             else:
-                BaseName = Name
+                raise TypeError(">> Invalid input. Expected a transform or nurbsCurve node name (string).")
+        else:
+            raise TypeError(">> Invalid input. Expected a string for curve name.")
 
-            Count = 1
-            while True:
-                NodeName = "{}_{}{}" .format(BaseName , Count , Suffix)
-                if not cmds.objExists(NodeName):
+        if normalization:
+            crv = cmds.rebuildCurve(crv, rebuildType=0, keepRange=0, endKnots=1,
+                                   keepControlPoints=1, replaceOriginal=True)
+            self.log("Curve rebuilt and normalized: {}".format(crv))
+
+        self.crv = crv
+        self.crv_shp = crv_shp
+        self.pocif_dic["Curve"] = self.crv
+        self.pocif_dic["CurveShape"] = self.crv_shp
+
+    def create_crv(self, name="", degree=1):
+        if len(self.points_for_make_crv) < 2:
+            raise ValueError(">> Not enough points to create curve. At least 2 points are required.")
+        
+        base_name = name if name else self.main_name
+        count = 1
+        candidate_name = base_name
+
+        while cmds.objExists(candidate_name):
+            candidate_name = "{}{}".format(base_name, count)
+            count += 1
+
+        self.crv = cmds.curve(n=candidate_name, d=degree, p=self.points_for_make_crv)
+        shapes = cmds.listRelatives(self.crv, s=True) or []
+        if not shapes:
+            raise RuntimeError(">> Failed to create curve shape for {}".format(self.crv))
+        self.crv_shp = cmds.rename(shapes[0], self.crv + "Shape")
+        self.pocif_dic["Curve"] = self.crv
+        self.pocif_dic["CurveShape"] = self.crv_shp
+        self.log("Created curve '{}' with shape '{}'".format(self.crv, self.crv_shp))
+
+    def create_pocifs(self, name="", suffix=None):
+        if not self.crv or not self.crv_shp:
+            raise RuntimeError(">> Curve and curve shape must be defined before creating pocif nodes.")
+
+        base_name = name if name else self.crv
+        suffix = suffix if suffix is not None else self.suffix_name
+        self.pocif_nodes = []
+
+        num_points = len(self.points_for_make_crv)
+        if num_points < 2:
+            raise ValueError(">> Need at least 2 points to create pocif nodes.")
+
+        count = 1
+        max_tries = 1000
+
+        for i in range(num_points):
+            parameter = i / float(num_points - 1)
+            tries = 0
+
+            while tries < max_tries:
+                node_name = "{}{}{}".format(base_name, count, suffix)
+                if not cmds.objExists(node_name):
                     break
-                Count +=1
-                
+                count += 1
+                tries += 1
+            else:
+                raise RuntimeError(">> Could not find a unique node name after {} attempts.".format(max_tries))
 
-                
-                
-                
+            node = cmds.createNode('pointOnCurveInfo', n=node_name)
+            cmds.connectAttr("{}.worldSpace[0]".format(self.crv_shp), "{}.inputCurve".format(node), f=True)
+            cmds.setAttr("{}.turnOnPercentage".format(node), 1)
+            cmds.setAttr("{}.parameter".format(node), parameter)
+            self.pocif_nodes.append(node)
+            count += 1
 
+        self.log("Created {} pointOnCurveInfo nodes.".format(len(self.pocif_nodes)))
 
+    def create_locs(self, name="", suffix=None):
+        if not self.pocif_nodes:
+            raise RuntimeError(">> No pocif nodes available to create locators.")
 
-Test = CreatePocifLoc()
-sel = cmds.ls(sl =1)
+        base_name = name if name else self.crv
+        suffix = suffix if suffix is not None else self.loc_suffix
 
-Test.Add_ObjForMakeCrvPos(sel , 1)
-Test.MakeCrv()
+        self.locs = []
+        self.pocif_dic["PocifLocs"] = []
+
+        count = 1
+        max_tries = 1000
+
+        for pocif_node in self.pocif_nodes:
+            tries = 0
+            while tries < max_tries:
+                loc_name = "{}{}{}".format(base_name, count, suffix)
+                if not cmds.objExists(loc_name):
+                    break
+                count += 1
+                tries += 1
+            else:
+                raise RuntimeError(">> Could not find a unique locator name after {} attempts.".format(max_tries))
+
+            loc = cmds.spaceLocator(n=loc_name)[0]
+            cmds.connectAttr("{}.position".format(pocif_node), "{}.translate".format(loc), f=True)
+            self.locs.append(loc)
+            self.pocif_dic["PocifLocs"].append({"locator": loc, "pocif_node": pocif_node})
+            count += 1
+
+        self.log("Created {} locators connected to pocif nodes.".format(len(self.locs)))
+
+# 사용 예
+test = CreatePocifLoc()
+sel = cmds.ls(sl=True)
+
+test.add_object_positions(sel, print_log=True)
+test.create_crv()
+test.create_pocifs()
+test.create_locs()
