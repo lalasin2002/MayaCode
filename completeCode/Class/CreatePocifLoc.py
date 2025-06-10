23bdaf31e785789b4935020c52c8725e5b90accc
bjh <lalasin2002@gmail.com>
2025-06-10 15:54:00 +0900

    CreatePocifLoc 제작중

---
+++ completeCode/Class/CreatePocifLoc.py
@@ -0,0 +1,10 @@
+import maya.cmds as cmds
+
+
+class CreatePocifLoc:
+    def __init__(self  ):
+        self.Crv = None
+        self.CrvShp = None
+        self.Locs = []
+        self.PocifNodes = []
+              
